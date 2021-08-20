c51d9cabbf334e0101a4a2974f22178cfa4e2373
gregoriomena <gregoriomena@hiberus.com>
2021-08-20 10:09:29 +0200

    Proyecto base

---
+++ src/main/java/com/hiberus/gmenar/twittertest/AccionaServicioTwittertestApplication.java
@@ -0,0 +1,13 @@
+package com.hiberus.gmenar.twittertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccionaServicioTwittertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccionaServicioTwittertestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiberus/gmenar/twittertest/AccionaServicioTwittertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiberus.gmenar.twittertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccionaServicioTwittertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
